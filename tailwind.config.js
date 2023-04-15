edba5127589a2f0aa171b65d531e7a61cd021ec4
Liliana Fu <lilifuye72@gmail.com>
2023-04-15 11:22:29 -0500

    fix:html fix elements

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,32 +2,29 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    colors: {
-      blue : '#2C45B4',
-      lightblue : '#009FDF',
-      orange: '#FF7152',
-      snow: '#F1F6FF',
-      white: '#FFFFFF',
-      black: '#000000'
-    },
     extend: {
       backgroundImage: {
         'hero': "url(/src/assets/pexels-karolina-grabowska-7195310.jpg)",
+      },
+      colors: {
+        primary : '#2C45B4',
+        secondary : '#009FDF',
+        contrast: '#FF7152',
+        snow: '#F1F6FF',
+        white: '#FFFFFF',
+        black: '#000000'
+      },
+      container: {
+        center:true,
+      },
+      fontSize: {
+        '16': '16px',
+        '24': '24px',
+        '32': '32px',
+        '48': '48px',
+        '56': '56px',
       }
     },
-    container: {
-      center:true,
-    },
-    fontSize: {
-      '16': '16px',
-      '24': '24px',
-      '32': '32px',
-      '48': '48px',
-      '56': '56px',
-    },
-    width: {
-      '18': '72px'
-    }
   },
   plugins: [],
 }
