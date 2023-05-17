10797ec1021f38b49859a235c50682ca3f0642a2
Liliana Fu <lilifuye72@gmail.com>
2023-05-17 11:33:22 -0500

    feat: add links to application

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,8 +1,36 @@
-const navLinks = document.querySelector('.responsive-navbar');
+const responsiveNavbar = document.querySelector('.responsive-navbar');
+const menuIcon = document.getElementById('menuIcon');
+
 function onToggleMenu(e){
     e.name = e.name === 'menu' ? 'close': 'menu';
-    navLinks.classList.remove('hidden');
+    responsiveNavbar.classList.remove('hidden');
     if (e.name === 'menu'){
-        navLinks.classList.add('hidden');
+        responsiveNavbar.classList.add('hidden');
     }
 }
+
+function closeToggleMenu(e){
+    menuIcon.name = menuIcon.name === 'menu' ? 'close': 'menu';
+    responsiveNavbar.classList.add('hidden');
+}
+
+const featuresSectionRef = document.getElementById("features-section");
+const customersSectionRef = document.getElementById("customers-section");
+const aboutSectionRef = document.getElementById("about-section");
+
+const navigate = (htmlElement, scrollLogicalPosition = "start") => htmlElement.scrollIntoView({ behavior: "smooth", block: scrollLogicalPosition });
+
+document.getElementById("features-nav-link").addEventListener("click", (event) => {
+    event.preventDefault();
+    navigate(featuresSectionRef);
+});
+
+document.getElementById("customers-nav-link").addEventListener("click", (event) => {
+    event.preventDefault();
+    navigate(customersSectionRef);
+});
+
+document.getElementById("about-nav-link").addEventListener("click", (event) => {
+    event.preventDefault();
+    navigate(aboutSectionRef);
+});
